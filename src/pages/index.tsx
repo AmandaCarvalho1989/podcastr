3cd78acf1e5bf673fd8451c4bbb73471b7b689e6
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2021-04-20 22:47:00 -0300

    add header and player components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>oi next</h1>;
+import { useEffect } from "react";
+import Header from "../components/Header";
+
+interface HomeProps {
+  episodes: [];
+}
+export default function Home({ episodes }) {
+  return <h1> Index</h1>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 68 * 68 * 8,
+  };
 }
