35c2c22327ca013e8b7f13322e97e747d2742ce9
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2021-04-21 21:54:51 -0300

    add axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
 import Header from "../components/Header";
+import { GetServerSideProps } from "next";
 
-interface HomeProps {
-  episodes: [];
-}
-export default function Home({ episodes }) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  episodes: Array<Episode>;
+};
+
+export default function Home(props: HomeProps) {
   return <h1> Index</h1>;
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
+export const getStaticProps: GetServerSideProps = async () => {
+  const response = await fetch(
+    "http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc"
+  );
 
   const data = await response.json();
 
@@ -19,4 +29,4 @@ export async function getStaticProps() {
     },
     revalidate: 68 * 68 * 8,
   };
-}
+};
