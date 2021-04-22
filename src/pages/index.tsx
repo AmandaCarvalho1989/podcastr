d888e1f6574e596f8c6ae186f34dc5d7615de575
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2021-04-22 17:59:49 -0300

    create episode page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import ptBR from "date-fns/locale/pt-BR";
 import Image from "next/image";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 import styles from "../styles/home.module.scss";
-
+import Link from "next/link";
 type Episode = {
   id: string;
   title: string;
@@ -14,7 +14,6 @@ type Episode = {
   publishedAt: string;
   duration: number;
   durationAsString: string;
-  description: string;
   url: string;
 };
 
@@ -40,7 +39,9 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
                   objectFit="cover"
                 />
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -57,18 +58,24 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
         <h2>Todos episódios</h2>
         <table cellSpacing={0}>
           <thead>
-            <th></th>
-            <th>Podcast</th>
-            <th>Integrantes</th>
-            <th>Data</th>
-            <th>Duração</th>
-            <th></th>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
             {allEpisodes.map((episode) => {
               return (
                 <tr key={episode.id}>
-                  <td>
+                  <td
+                    style={{
+                      width: 72,
+                    }}
+                  >
                     <Image
                       width={120}
                       height={120}
@@ -78,10 +85,18 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
                     />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td>{episode.members}</td>
-                  <td>{episode.publishedAt}</td>
+                  <td
+                    style={{
+                      width: 100,
+                    }}
+                  >
+                    {episode.publishedAt}
+                  </td>
                   <td>{episode.durationAsString}</td>
                   <td>
                     <button>
@@ -120,7 +135,7 @@ export const getStaticProps: GetServerSideProps = async () => {
       durationAsString: convertDurationToTimeString(
         Number(episode.file.duration)
       ),
-      description: episode.description,
+
       url: episode.file.url,
     };
   });
