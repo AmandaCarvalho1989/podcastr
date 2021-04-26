4dd841a5f707fcf033e977bbbc9f73378ed8f517
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2021-04-26 10:05:12 -0300

    update for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
-
-const api = axios.create({
-  baseURL: 'http://localhost:3333/'
-})
-
-export { api }
+export const api = axios.create({
+  baseURL: process.env.API_URL || "http://localhost:3333/",
+});
